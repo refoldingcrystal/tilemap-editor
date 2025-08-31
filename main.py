15af152f9d0a46c67249f5bb58aa9e4b294d534f
refoldingcrystal <meow>
2025-08-31 22:37:23 +0200

    tilemap grid

--- main.py
+++ main.py
@@ -1,70 +1,13 @@
-import json
 import os
 import sys
 import pygame
+
+from tilemap import Tilemap
 
 
 def close():
     pygame.quit()
     sys.exit()
-
-def pos2str(pos):
-    return str(pos[0]) + ';' + str(pos[1])
-
-def str2pos(pos):
-    pos = pos.split(';')
-    return (int(pos[0]), int(pos[1]))
-
-
-class Tilemap:
-    def __init__(self, surf):
-        self.surf = surf
-        self.tilemap = {}
-        self.tile_size = 50
-        self.tile_type = 1
-        self.colors = [
-            (255, 0, 0),
-            (0, 255, 0),
-            (0, 0, 255),
-            (0, 255, 255),
-            (255, 0, 255),
-            (255, 255, 0)
-        ]
-
-    def clear(self):
-        self.tilemap = {}
-
-    def change_tile_type(self, movement):
-        self.tile_type = (self.tile_type + movement - 1) % len(self.colors) + 1
-    
-    def mouse2pos(self, m_pos):
-        return (m_pos[0] // self.tile_size, m_pos[1] // self.tile_size)
-
-    def add(self, m_pos):
-        self.tilemap[pos2str(self.mouse2pos(m_pos))] = self.tile_type
-
-    def remove(self, m_pos):
-        pos = pos2str(self.mouse2pos(m_pos))
-        if pos in self.tilemap:
-            del self.tilemap[pos2str(self.mouse2pos(m_pos))]
-
-    def render(self, m_pos):
-        for tile_pos in self.tilemap:
-            pos = str2pos(tile_pos)
-            rect = (pos[0] * self.tile_size, pos[1] * self.tile_size,
-                    self.tile_size, self.tile_size)
-            color = self.colors[self.tilemap[tile_pos] - 1]
-            pygame.draw.rect(self.surf, color, rect)
-        
-        pos = self.mouse2pos(m_pos)
-        rect = (pos[0] * self.tile_size, pos[1] * self.tile_size,
-                self.tile_size, self.tile_size)
-        color = self.colors[self.tile_type - 1]
-        pygame.draw.rect(self.surf, color, rect, width=5)
-
-    def export(self, filename):
-        with open(filename, 'w') as file:
-            json.dump(self.tilemap, file)
         
 
 class Window:
@@ -86,6 +29,8 @@ class Window:
                         self.tilemap.export(self.filename)
                     if event.key == pygame.K_c:
                         self.tilemap.clear()
+                    if event.key == pygame.K_g:
+                        self.tilemap.draw_grid ^= True
                 if event.type == pygame.MOUSEWHEEL:
                     self.tilemap.change_tile_type(event.y)
 
@@ -100,18 +45,21 @@ class Window:
             self.tilemap.render(m_pos)
             pygame.display.update()
 
+suppress_warning = True
+
 if len(sys.argv) < 2:
     print("Using 'map.json' as output file")
     filename = 'map.json'
-# else:
-#     filename = sys.argv[1]
-# if os.path.isdir(filename):
-#     print(f"'{filename} is a directory")
-#     sys.exit()
-# if os.path.isfile(filename):
-#     print(f"'{filename}' is a file, continue anyway? [Y/n]")
-#     if input() not in ['', 'y', 'Y']:
-#         sys.exit()
+else:
+    filename = sys.argv[1]
+if os.path.isdir(filename):
+    print(f"'{filename} is a directory")
+    sys.exit()
+if os.path.isfile(filename):
+    if not suppress_warning:
+        print(f"'{filename}' is a file, continue anyway? [Y/n]")
+        if input() not in ['', 'y', 'Y']:
+            sys.exit()
 
 Window(filename).run()
 
---
+++ tilemap.py
@@ -0,0 +1,71 @@
+import json
+import pygame
+
+
+def pos2str(pos):
+    return str(pos[0]) + ';' + str(pos[1])
+
+def str2pos(pos):
+    pos = pos.split(';')
+    return (int(pos[0]), int(pos[1]))
+
+
+class Tilemap:
+    def __init__(self, surf):
+        self.draw_grid = True
+        self.surf = surf
+        self.tilemap = {}
+        self.tile_size = 50
+        self.tile_type = 1
+        self.colors = [
+            (255, 0, 0),
+            (0, 255, 0),
+            (0, 0, 255),
+            (0, 255, 255),
+            (255, 0, 255),
+            (255, 255, 0)
+        ]
+
+    def clear(self):
+        self.tilemap = {}
+
+    def change_tile_type(self, movement):
+        self.tile_type = (self.tile_type + movement - 1) % len(self.colors) + 1
+    
+    def mouse2pos(self, m_pos):
+        return (m_pos[0] // self.tile_size, m_pos[1] // self.tile_size)
+
+    def add(self, m_pos):
+        self.tilemap[pos2str(self.mouse2pos(m_pos))] = self.tile_type
+
+    def remove(self, m_pos):
+        pos = pos2str(self.mouse2pos(m_pos))
+        if pos in self.tilemap:
+            del self.tilemap[pos2str(self.mouse2pos(m_pos))]
+
+    def render(self, m_pos):
+        for tile_pos in self.tilemap:
+            pos = str2pos(tile_pos)
+            rect = (pos[0] * self.tile_size, pos[1] * self.tile_size,
+                    self.tile_size, self.tile_size)
+            color = self.colors[self.tilemap[tile_pos] - 1]
+            pygame.draw.rect(self.surf, color, rect)
+
+        if self.draw_grid:
+            for x in range(self.surf.get_width() // self.tile_size + 1):
+                pygame.draw.line(self.surf, (200, 200, 200),
+                                (x * self.tile_size, 0), (x * self.tile_size, self.surf.get_height()))
+            for y in range(self.surf.get_height() // self.tile_size + 1):
+                pygame.draw.line(self.surf, (200, 200, 200),
+                                (0, y * self.tile_size), (self.surf.get_width(), y * self.tile_size))
+        
+        pos = self.mouse2pos(m_pos)
+        rect = (pos[0] * self.tile_size, pos[1] * self.tile_size,
+                self.tile_size, self.tile_size)
+        color = self.colors[self.tile_type - 1]
+        pygame.draw.rect(self.surf, color, rect, width=5)
+
+    def export(self, filename):
+        with open(filename, 'w') as file:
+            json.dump(self.tilemap, file)
+        print(f"Exported to '{filename}'")
